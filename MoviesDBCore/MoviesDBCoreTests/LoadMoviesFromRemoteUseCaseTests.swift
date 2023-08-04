63b921d3188bd9d3b66d099e9b4d14e84bb4f798
amsow <amadou.d.sow@gmail.com>
2023-08-04 16:13:43 +0000

    `RemoteMoviesLoader.load` delivers error on client error

--- MoviesDBCore/MoviesDBCoreTests/LoadMoviesFromRemoteUseCaseTests.swift
+++ MoviesDBCore/MoviesDBCoreTests/LoadMoviesFromRemoteUseCaseTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 protocol HTTPClient {
     
-    func request(from url: URL)
+    func request(from url: URL, completion: @escaping (Error?) -> Void)
 }
 
 final class RemoteMoviesLoader {
@@ -16,8 +16,8 @@ final class RemoteMoviesLoader {
         self.client = client
     }
     
-    func load() {
-        client.request(from: url)
+    func load(completion: @escaping (Error?) -> Void) {
+        client.request(from: url, completion: completion)
     }
 }
 
@@ -38,7 +38,7 @@ final class LoadMoviesFromRemoteUseCaseTests: XCTestCase {
         let (client, sut) = makeSUT(url: url)
         
         // When
-        sut.load()
+        sut.load { _ in }
         
         // Then
         XCTAssertEqual(client.requestedURLs, [url])
@@ -50,11 +50,27 @@ final class LoadMoviesFromRemoteUseCaseTests: XCTestCase {
         let (client, sut) = makeSUT(url: url)
         
         // When
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         // Then
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (client, sut) = makeSUT()
+        let anyError = NSError(domain: "any error", code: 0)
+        let loadCompletionExpectation = expectation(description: "Wait for load completion")
+        
+        sut.load { error in
+            XCTAssertNotNil(error)
+            XCTAssertEqual(anyError, error as? NSError)
+            
+            loadCompletionExpectation.fulfill()
+        }
+        
+        client.complete(with: anyError)
+        wait(for: [loadCompletionExpectation], timeout: 1.0)
     }
     
     // MARK: - Helpers
@@ -69,9 +85,15 @@ final class LoadMoviesFromRemoteUseCaseTests: XCTestCase {
     
     final class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs = [URL]()
+        private var errorCompletions = [(Error?) -> Void]()
         
-         func request(from url: URL) {
+        func request(from url: URL, completion: @escaping (Error?) -> Void) {
             requestedURLs.append(url)
+            errorCompletions.append(completion)
+        }
+        
+        func complete(with error: Error) {
+            errorCompletions[0](error)
         }
     }
 }
