48b24945ece2a6caede5dfcb513fd8ec76c3565f
amsow <amadou.d.sow@gmail.com>
2023-08-10 12:46:19 +0000

    Requests cache deletion on save command

--- MoviesDBCore/MoviesDBCoreTests/CacheMoviesUseCaseTests.swift
+++ MoviesDBCore/MoviesDBCoreTests/CacheMoviesUseCaseTests.swift
@@ -1,12 +1,25 @@
 
 import XCTest
 
+protocol MoviesStore {
+    func deleteCachedMovies()
+}
+
 final class LocalMoviesLoader {
-    init(store: Any) {}
+    
+    private let store: MoviesStore
+    
+    init(store: MoviesStore) {
+        self.store = store
+    }
+    
+    func save() {
+        store.deleteCachedMovies()
+    }
 }
 
 final class CacheMoviesUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotMessageCacheUponCreation() {
         let store = MoviesStoreSpy()
         _ = LocalMoviesLoader(store: store)
@@ -14,7 +27,24 @@ final class CacheMoviesUseCaseTests: XCTestCase {
         XCTAssertTrue(store.messages.isEmpty)
     }
     
-    final class MoviesStoreSpy {
-        var messages = [String]()
+    func test_save_requestsCacheDeletion() {
+        let store = MoviesStoreSpy()
+        let sut = LocalMoviesLoader(store: store)
+        
+        sut.save()
+        
+        XCTAssertEqual(store.messages, [.deleteCachedMovies])
+    }
+    
+    final class MoviesStoreSpy: MoviesStore {
+        var messages = [Message]()
+        
+        enum Message {
+            case deleteCachedMovies
+        }
+        
+        func deleteCachedMovies() {
+            messages.append(.deleteCachedMovies)
+        }
     }
 }
