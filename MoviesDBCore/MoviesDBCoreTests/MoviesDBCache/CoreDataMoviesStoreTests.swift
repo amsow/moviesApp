f3cff31a0d76bd28663b5dd328984afb3c1ed3d9
amsow <amadou.d.sow@gmail.com>
2023-08-14 14:16:05 +0000

    `CoreDataMoviesStore.retrieve` delivers empty on empty cache

---
+++ MoviesDBCore/MoviesDBCoreTests/MoviesDBCache/CoreDataMoviesStoreTests.swift
@@ -0,0 +1,39 @@
+
+
+import XCTest
+import MoviesDBCore
+
+final class CoreDataMoviesStore: MoviesStore {
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success([]))
+    }
+    
+    func deleteCachedMovies(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func insert(_ movies: [MoviesDBCore.Movie], completion: @escaping InsertionCompletion) {
+        
+    }
+}
+
+final class CoreDataMoviesStoreTests: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CoreDataMoviesStore()
+        
+        let exp = expectation(description: "Wait for retrieve completion")
+        sut.retrieve { receivedResult in
+            if case .success(let cachedMovies) = receivedResult {
+                XCTAssertTrue(cachedMovies.isEmpty)
+            } else {
+                XCTFail("Should received a success with an empty movies array, got \(receivedResult) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
