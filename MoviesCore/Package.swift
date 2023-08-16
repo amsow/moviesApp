e2f696fba866807877d69eca2cc8e772fdc28e0e
amsow <amadou.d.sow@gmail.com>
2023-08-16 14:49:16 +0000

    Create `MoviesCore` package to replace the framework MoviesDBCore one

---
+++ MoviesCore/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MoviesCore",
+    platforms: [.macOS(.v12), .iOS(.v13)],
+    products: [
+        .library(
+            name: "MoviesCore",
+            type: .static,
+            targets: ["MoviesCore"]),
+    ],
+    targets: [
+        .target(
+            name: "MoviesCore",
+            dependencies: []),
+        .testTarget(
+            name: "MoviesCoreTests",
+            dependencies: ["MoviesCore"]),
+    ]
+)
