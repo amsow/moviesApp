35b6a4bd80f712ce042cb76c2d60e14b7127ab65
amsow <amadou.d.sow@gmail.com>
2023-08-21 16:38:13 +0000

    `MoviesViewController` load movies on viewDidLoad

--- MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
+++ MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
@@ -1,19 +1,43 @@
 
 
 import XCTest
+import UIKit
 
-final class MoviesViewController {
-    init(loader: Any) {}
+final class MoviesViewController: UIViewController {
+    
+    private let loader: MoviesViewControllerTests.LoaderSpy
+    
+    init(loader: MoviesViewControllerTests.LoaderSpy) {
+        self.loader = loader
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader.load()
+    }
 }
 
 final class MoviesViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoad() {
         let loader = LoaderSpy()
-        let sut = MoviesViewController(loader: loader)
+        _ = MoviesViewController(loader: loader)
         
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_loadsMovies() {
+        let loader = LoaderSpy()
+        let sut = MoviesViewController(loader: loader)
         
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
     
     
@@ -21,5 +45,9 @@ final class MoviesViewControllerTests: XCTestCase {
     
     final class LoaderSpy {
         private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
