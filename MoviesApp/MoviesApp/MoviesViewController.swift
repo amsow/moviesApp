dbad7dc551b1346c35bdb4d19abe4d9091cf7fd6
amsow <amadou.d.sow@gmail.com>
2023-08-21 17:58:23 +0000

    Move `MoviesViewController` to its own file in production

---
+++ MoviesApp/MoviesApp/MoviesViewController.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+import MoviesCore
+
+public final class MoviesViewController: UITableViewController {
+    
+    private let loader: MoviesLoader
+    
+    public init(loader: MoviesLoader) {
+        self.loader = loader
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(loadMovies), for: .valueChanged)
+        loadMovies()
+    }
+    
+    @objc
+    private func loadMovies() {
+        loader.load { _ in }
+    }
+}
--- MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
+++ MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
@@ -1,34 +1,8 @@
 
 
 import XCTest
-import UIKit
 import MoviesCore
-
-final class MoviesViewController: UITableViewController {
-    
-    private let loader: MoviesLoader
-    
-    init(loader: MoviesLoader) {
-        self.loader = loader
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(loadMovies), for: .valueChanged)
-        loadMovies()
-    }
-    
-    @objc
-    private func loadMovies() {
-        loader.load { _ in }
-    }
-}
+import MoviesApp
 
 final class MoviesViewControllerTests: XCTestCase {
 
