f59fa8dce2eec06e0c0e49b73ec93cf4d0b1c46d
amsow <amadou.d.sow@gmail.com>
2023-08-22 13:08:31 +0000

    `MoviePosterImageDataLoader` does not perform any url request on init

--- MoviesCore/Tests/MoviesCoreTests/MoviesAPI/LoadMoviePosterImageDataFromRemoteUseCaseTests.swift
+++ MoviesCore/Tests/MoviesCoreTests/MoviesAPI/LoadMoviePosterImageDataFromRemoteUseCaseTests.swift
@@ -2,7 +2,22 @@
 
 import XCTest
 
+final class MoviePosterImageDataLoader {
+    init(client: Any) {}
+}
+
 final class LoadMoviePosterImageDataFromRemoteUseCaseTests: XCTestCase {
-
-
+   
+    func test_init_doesNotPerformAnyURLLoadRequest() {
+        let client = HTTPClientSpy()
+        _ = MoviePosterImageDataLoader(client: client)
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private final class HTTPClientSpy {
+        private(set) var requestedURLs = [URL]()
+    }
 }
