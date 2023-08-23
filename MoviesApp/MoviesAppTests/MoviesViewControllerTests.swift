f5e1536dda6a571e03ee3c221d45ce4a46554690
amsow <amadou.d.sow@gmail.com>
2023-08-23 22:18:26 +0000

    Extract test helpers into their own files (MoviesViewController and MovieCell)

--- MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
+++ MoviesApp/MoviesAppTests/MoviesViewControllerTests.swift
@@ -381,66 +381,6 @@ final class MoviesViewControllerTests: XCTestCase {
     }
 }
 
-extension MoviesViewController {
-    private var section: Int { return 0 }
-    
-    func simulateUserInitiatedMoviesReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    @discardableResult
-    func simulateMovieViewVisible(at index: Int) -> MovieCell? {
-        return movieCell(at: index) as? MovieCell
-    }
-    
-    func simulateMovieViewNearVisible(at index: Int) {
-        let datasource = tableView.prefetchDataSource
-        datasource?.tableView(tableView, prefetchRowsAt: [IndexPath(row: index, section: 0)])
-    }
-    
-    func simulateMovieViewNotNearVisibleAnymore(at index: Int) {
-        let datasource = tableView.prefetchDataSource
-        datasource?.tableView?(tableView, cancelPrefetchingForRowsAt: [IndexPath(row: index, section: section)])
-    }
-    
-    func simulateMovieViewNotVisible(at index: Int) {
-        let delegate = tableView.delegate
-        guard let cell = simulateMovieViewVisible(at: index) else { return }
-        delegate?.tableView?(tableView, didEndDisplaying: cell, forRowAt: IndexPath(row: index, section: section))
-    }
-    
-    func isShowingLoadingIndicator() -> Bool {
-        refreshControl?.isRefreshing == true
-    }
-    
-    func numberOfRenderedMovieViews() -> Int {
-        tableView.numberOfRows(inSection: section)
-    }
-    
-    func movieCell(at row: Int) -> UITableViewCell? {
-        let datasource = tableView.dataSource
-       return datasource?.tableView(tableView, cellForRowAt: IndexPath(row: row, section: section))
-    }
-}
-
-extension MovieCell {
-    func isShowingLoadingIndicator() -> Bool {
-        return posterImageContainer.isShimmering
-    }
-    
-    func isRetryButtonVisibleOnImageView() -> Bool {
-        return retryButton.isHidden == false
-    }
-    
-    func simulateRetryAction() {
-        retryButton.simulate(event: .touchUpInside)
-    }
-    
-    var renderedImageData: Data? {
-        posterImageView.image?.pngData()
-    }
-}
-
 extension Date {
     func year() -> String {
         let calendar = Calendar(identifier: .gregorian)
