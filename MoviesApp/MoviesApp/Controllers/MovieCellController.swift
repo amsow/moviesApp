e5800f0a399b99153bb022cf4f6a4bc6f0cfb4c2
amsow <amadou.d.sow@gmail.com>
2023-08-25 12:25:14 +0000

    Add `MovieCellPresenter` for presentation purpose for each state of loading movies(start, finish with data, finish with error), prepare the data make it view presentable and pass it to the associated view

--- MoviesApp/MoviesApp/Controllers/MovieCellController.swift
+++ MoviesApp/MoviesApp/Controllers/MovieCellController.swift
@@ -6,7 +6,7 @@ struct MovieViewModel<Image> {
     let title: String
     let overview: String
     let releaseDate: String?
-    let posterImage: Image
+    let posterImage: Image?
     let isLoading: Bool
     let shouldRetry: Bool
 }
@@ -16,6 +16,53 @@ protocol MovieCellPresentable {
     func display(_ viewModel: MovieViewModel<Image>)
 }
 
+final class MovieCellPresenter<View: MovieCellPresentable, Image> where View.Image == Image {
+    
+    private var view: View
+    private let imageTransformer: (Data) -> Image?
+    
+    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    func didStartLoadingImageData(for model: Movie) {
+        view.display(
+            MovieViewModel(
+                title: model.title,
+                overview: model.overview,
+                releaseDate: model.releaseDate.year(),
+                posterImage: nil,
+                isLoading: true,
+                shouldRetry: false)
+        )
+    }
+    
+    func didFinishLoadingImageDataWithData(_ data: Data, for model: Movie) {
+        let img = imageTransformer(data)
+        view.display(
+            MovieViewModel(
+                title: model.title,
+                overview: model.overview,
+                releaseDate: model.releaseDate.year(),
+                posterImage: img,
+                isLoading: false,
+                shouldRetry: img == nil)
+        )
+    }
+    
+    func didFinishLoadingImageDataWithError(_ error: Error, for model: Movie) {
+        view.display(
+            MovieViewModel(
+                title: model.title,
+                overview: model.overview,
+                releaseDate: model.releaseDate.year(),
+                posterImage: nil,
+                isLoading: false,
+                shouldRetry: true)
+        )
+    }
+}
 final class MovieCellController {
     
     /// Properties to manage the state of the controller
