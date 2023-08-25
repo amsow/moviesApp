55519be516d00b8ed9219f6e972a46a06a7bd73b
amsow <amadou.d.sow@gmail.com>
2023-08-25 13:17:48 +0000

    Move presentation purpose classes or object to their own files: `MovieViewModel`, `MovieCellPresenter` and group in Presentation folder as they are and should be framework agnostic they do not depend an framework specific element

--- MoviesApp/MoviesApp/Controllers/MovieCellController.swift
+++ MoviesApp/MoviesApp/Controllers/MovieCellController.swift
@@ -2,68 +2,6 @@
 import UIKit
 import MoviesCore
 
-struct MovieViewModel<Image> {
-    let title: String
-    let overview: String
-    let releaseDate: String?
-    let posterImage: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-}
-
-protocol MovieCellPresentable: AnyObject {
-    associatedtype Image
-    func display(_ viewModel: MovieViewModel<Image>)
-}
-
-final class MovieCellPresenter<View: MovieCellPresentable, Image> where View.Image == Image {
-    
-    private weak var view: View?
-    private let imageTransformer: (Data) -> Image?
-    
-    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
-        self.view = view
-        self.imageTransformer = imageTransformer
-    }
-    
-    func didStartLoadingImageData(for model: Movie) {
-        view?.display(
-            MovieViewModel(
-                title: model.title,
-                overview: model.overview,
-                releaseDate: model.releaseDate.year(),
-                posterImage: nil,
-                isLoading: true,
-                shouldRetry: false)
-        )
-    }
-    
-    func didFinishLoadingImageDataWithData(_ data: Data, for model: Movie) {
-        let img = imageTransformer(data)
-        view?.display(
-            MovieViewModel(
-                title: model.title,
-                overview: model.overview,
-                releaseDate: model.releaseDate.year(),
-                posterImage: img,
-                isLoading: false,
-                shouldRetry: img == nil)
-        )
-    }
-    
-    func didFinishLoadingImageDataWithError(_ error: Error, for model: Movie) {
-        view?.display(
-            MovieViewModel(
-                title: model.title,
-                overview: model.overview,
-                releaseDate: model.releaseDate.year(),
-                posterImage: nil,
-                isLoading: false,
-                shouldRetry: true)
-        )
-    }
-}
-
 final class MovieCellController: MovieCellPresentable {
     
     /// Properties to manage the state of the controller
