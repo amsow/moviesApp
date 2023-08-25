fc6490e2d00fe107ba551276de71bbb78961046c
amsow <amadou.d.sow@gmail.com>
2023-08-25 15:43:10 +0000

    Move `MoviePosterImageDataLoaderPresentationAdapter` to its own file

--- MoviesApp/MoviesApp/Controllers/MovieCellController.swift
+++ MoviesApp/MoviesApp/Controllers/MovieCellController.swift
@@ -7,40 +7,6 @@ protocol MovieCellControllerDelegate {
     func didCancelImageDataLoadingRequest()
 }
 
-final class MoviePosterImageDataLoaderPresentationAdapter<View: MovieCellPresentable, Image>: MovieCellControllerDelegate where View.Image == Image {
-    
-    private let model: Movie
-    private let imageDataLoader: ImageDataLoader
-    private var imageDataLoaderTask: ImageDataLoaderTask?
-    
-    var presenter: MovieCellPresenter<View, Image>?
-    
-    init(model: Movie, imageDataLoader: ImageDataLoader) {
-        self.model = model
-        self.imageDataLoader = imageDataLoader
-    }
-    
-    func didRequestImageDataLoading() {
-        let model = self.model
-        presenter?.didStartLoadingImageData(for: model)
-        imageDataLoaderTask = imageDataLoader.loadImageData(from: model.posterImageURL) { [weak self] result in
-            guard let self else { return }
-            switch result {
-            case .success(let data):
-                presenter?.didFinishLoadingImageDataWithData(data, for: model)
-                
-            case .failure(let error):
-                presenter?.didFinishLoadingImageDataWithError(error, for: model)
-            }
-        }
-    }
-    
-    func didCancelImageDataLoadingRequest() {
-        imageDataLoaderTask?.cancel()
-        imageDataLoaderTask = nil
-    }
-}
-
 final class MovieCellController: MovieCellPresentable {
     
     private var cell: MovieCell?
