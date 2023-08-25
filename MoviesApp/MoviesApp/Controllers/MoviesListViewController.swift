622a6368bae8cb46ae0ec4e6351edd9eaaef5527
amsow <amadou.d.sow@gmail.com>
2023-08-25 16:31:18 +0000

    Move `WeakReferenceProxy` to its own file.

--- MoviesApp/MoviesApp/Controllers/MoviesListViewController.swift
+++ MoviesApp/MoviesApp/Controllers/MoviesListViewController.swift
@@ -104,17 +104,3 @@ extension Date {
         return yearComponent.description
     }
 }
-
-final class WeakReferenceProxy<Object: AnyObject> {
-    private weak var object: Object?
-
-    init(_ object: Object) {
-        self.object = object
-    }
-}
-
-extension WeakReferenceProxy: MovieCellPresentable where Object: MovieCellPresentable, Object.Image == UIImage {
-    func display(_ viewModel: MovieViewModel<UIImage>) {
-        object?.display(viewModel)
-    }
-}
