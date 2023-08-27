4961744d8b0c7f4a4a552ca370e7aca51c7e70f1
amsow <amadou.d.sow@gmail.com>
2023-08-27 12:31:20 +0000

    `LocalMoviePosterImageDataLoader.loadImageDataFromURL` request stored data for url: replace the movies store spy with the specific image data store spy as its does not depend on the same interface and behavior (ISP principle)

--- MoviesCore/Tests/MoviesCoreTests/MoviesCache/LocalMoviePosterImageDataFromCacheUseCaseTests.swift
+++ MoviesCore/Tests/MoviesCoreTests/MoviesCache/LocalMoviePosterImageDataFromCacheUseCaseTests.swift
@@ -1,10 +1,23 @@
 
 
 import XCTest
+import MoviesCore
+
+protocol ImageDataStore {
+    func retrieveData(for url: URL)
+}
 
 final class LocalMoviePosterImageDataLoader {
     
-    init(store: Any) {}
+    private let store: ImageDataStore
+    
+    init(store: ImageDataStore) {
+        self.store = store
+    }
+    
+    func loadImageData(from url: URL) {
+        store.retrieveData(for: url)
+    }
 }
 
 final class LocalMoviePosterImageDataFromCacheUseCaseTests: XCTestCase {
@@ -15,14 +28,35 @@ final class LocalMoviePosterImageDataFromCacheUseCaseTests: XCTestCase {
         XCTAssertTrue(store.messages.isEmpty)
     }
     
+    func test_loadImageDataFromURL_requestsStoredDataForURL() {
+        let (store, sut) = makeSUT()
+        
+        let url = anyURL()
+        sut.loadImageData(from: url)
+        
+        XCTAssertEqual(store.messages, [.retrieve(dataForURL: url)])
+    }
+    
     // MARK: - Private Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (store: MoviesStoreSpy, sut: LocalMoviePosterImageDataLoader) {
-        let store = MoviesStoreSpy()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (store: ImageDataStoreSpy, sut: LocalMoviePosterImageDataLoader) {
+        let store = ImageDataStoreSpy()
         let sut = LocalMoviePosterImageDataLoader(store: store)
         trackMemoryLeaks(store, file: file, line: line)
         trackMemoryLeaks(sut, file: file, line: line)
         
         return (store, sut)
+    }
+    
+    final class ImageDataStoreSpy: ImageDataStore {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case retrieve(dataForURL: URL)
+        }
+        
+        func retrieveData(for url: URL) {
+            messages.append(.retrieve(dataForURL: url))
+        }
     }
 }
