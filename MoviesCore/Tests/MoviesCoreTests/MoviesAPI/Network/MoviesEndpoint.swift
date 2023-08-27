a1d11d63456d614d1698fe1a4855e6c8162d0b7a
amsow <amadou.d.sow@gmail.com>
2023-08-27 20:33:38 +0000

    Configure movies endpoint to setup the url properly

---
+++ MoviesCore/Tests/MoviesCoreTests/MoviesAPI/Network/MoviesEndpoint.swift
@@ -0,0 +1,36 @@
+
+import Foundation
+
+struct NetworkConfig {
+    static var apiBaseURL: NSString = "https://api.themoviedb.org/3"
+    
+    static var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+            assertionFailure("API_KEY not found")
+            return ""
+        }
+        return apiKey
+    }()
+}
+
+struct MoviesEndpoint {
+    
+    static var path: String {
+        return "discover/movie"
+    }
+    
+    static func url() -> URL? {
+        let urlString = NetworkConfig.apiBaseURL.appendingPathComponent(path)
+        var urlComponents = URLComponents(string: urlString)
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "api_key", value: NetworkConfig.apiKey)
+        ]
+        
+        guard let url = urlComponents?.url else {
+            assertionFailure("Unable to configure url with path \(path) and baseURL \(NetworkConfig.apiBaseURL)")
+            return nil
+        }
+        
+        return url
+    }
+}
