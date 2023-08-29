5b42ec619739b06365e12dcd91ab43cbf24af52c
amsow <amadou.d.sow@gmail.com>
2023-08-29 17:35:13 +0000

    movies list with failed image loading snapshot

--- MoviesApp/MoviesAppTests/MoviesListSnapshotTests.swift
+++ MoviesApp/MoviesAppTests/MoviesListSnapshotTests.swift
@@ -5,7 +5,7 @@ import MoviesCore
 @testable import MoviesApp
 
 final class MoviesListSnapshotTests: XCTestCase {
-
+    
     func test_emptyMoviesList() {
         let sut = makeSUT()
         
@@ -31,6 +31,14 @@ final class MoviesListSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), record: true, named: "MOVIES_LIST_ERROR_LIGHT")
     }
     
+    func test_moviesListWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.displayWithFailedImageLoading()
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), record: true, named: "MOVIES_LIST_WITH_FAILED_IMAGE_LOADING_light")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> MoviesListViewController {
@@ -45,43 +53,43 @@ final class MoviesListSnapshotTests: XCTestCase {
     
     
     func assert(snapshot: UIImage, record isRecording: Bool, named name: String, file: StaticString = #file, line: UInt = #line) {
-       let snapshotURL = makeSnapshotURL(named: name, file: file)
-       let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
+        let snapshotURL = makeSnapshotURL(named: name, file: file)
+        let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
         
         if isRecording {
             record(snapshot: snapshot, named: name, file: file, line: line)
         }
-       
-       guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
-           XCTFail("Failed to load store snapshot at URL: \(snapshotURL). Use the `record` method to store a snapshot before asserting.", file: file, line: line)
-           return
-       }
-       
-       if storedSnapshotData != snapshotData {
-           let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
-               .appendingPathComponent(snapshotURL.lastPathComponent)
-           
-           try? snapshotData?.write(to: temporarySnapshotURL)
-           
-           XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), stored snapshot URL: \(snapshotURL)", file: file, line: line)
-       }
-   }
-   
-   private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
-       let snapshotURL = makeSnapshotURL(named: name, file: file)
-       let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
-       
-       do {
-           try FileManager.default.createDirectory(
-               at: snapshotURL.deletingLastPathComponent(),
-               withIntermediateDirectories: true
-           )
-           
-           try snapshotData?.write(to: snapshotURL)
-       } catch {
-           XCTFail("Failed to record snapshot with error \(error)", file: file, line: line)
-       }
-   }
+        
+        guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
+            XCTFail("Failed to load store snapshot at URL: \(snapshotURL). Use the `record` method to store a snapshot before asserting.", file: file, line: line)
+            return
+        }
+        
+        if storedSnapshotData != snapshotData {
+            let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+                .appendingPathComponent(snapshotURL.lastPathComponent)
+            
+            try? snapshotData?.write(to: temporarySnapshotURL)
+            
+            XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), stored snapshot URL: \(snapshotURL)", file: file, line: line)
+        }
+    }
+    
+    private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
+        let snapshotURL = makeSnapshotURL(named: name, file: file)
+        let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
+        
+        do {
+            try FileManager.default.createDirectory(
+                at: snapshotURL.deletingLastPathComponent(),
+                withIntermediateDirectories: true
+            )
+            
+            try snapshotData?.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error \(error)", file: file, line: line)
+        }
+    }
     
     private func makeSnapshotURL(named name: String, file: StaticString) -> URL {
         let snapshotURL = URL(fileURLWithPath: String(describing: file))
@@ -108,34 +116,34 @@ extension MoviesListViewController {
     
     func displayMoviesListWithContent() {
         let cellstubs = [
-           MovieCellControllerStub(
-            title: "Black Panther",
-            overview: "Black panther full description",
-            releaseDate: "2019",
-            isLoading: false,
-            image: UIImage.makeWithColor(.red)
-           ),
-           MovieCellControllerStub(
-            title: "Casa De Papel",
-            overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-            releaseDate: "2019",
-            isLoading: false,
-            image: UIImage.makeWithColor(.green)
-           ),
-           MovieCellControllerStub(
-            title: "Glory",
-            overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            releaseDate: "2000",
-            isLoading: false,
-            image: UIImage.makeWithColor(.yellow)
-           ),
-           MovieCellControllerStub(
-            title: "les 100",
-            overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-            releaseDate: "1998",
-            isLoading: false,
-            image: UIImage.makeWithColor(.purple)
-           )
+            MovieCellControllerStub(
+                title: "Black Panther",
+                overview: "Black panther full description",
+                releaseDate: "2019",
+                isLoading: false,
+                image: UIImage.makeWithColor(.red)
+            ),
+            MovieCellControllerStub(
+                title: "Casa De Papel",
+                overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                releaseDate: "2019",
+                isLoading: false,
+                image: UIImage.makeWithColor(.green)
+            ),
+            MovieCellControllerStub(
+                title: "Glory",
+                overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                releaseDate: "2000",
+                isLoading: false,
+                image: UIImage.makeWithColor(.yellow)
+            ),
+            MovieCellControllerStub(
+                title: "les 100",
+                overview: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+                releaseDate: "1998",
+                isLoading: false,
+                image: UIImage.makeWithColor(.purple)
+            )
         ]
         
         display(cellstubs.map { stub in
@@ -155,6 +163,31 @@ extension MoviesListViewController {
         
         viewModel?.onLoadFailed?(message)
     }
+    
+    func displayWithFailedImageLoading() {
+        let cellsStubs = [
+            MovieCellControllerStub(
+                title: "Black Panther",
+                overview: "overview 1",
+                releaseDate: "2019",
+                isLoading: false,
+                image: nil
+            ),
+            MovieCellControllerStub(
+                title: "Casa De Papel",
+                overview: "overview 2",
+                releaseDate: "2023",
+                isLoading: false,
+                image: nil
+            )
+        ]
+        
+        display(cellsStubs.map { stub in
+            let cellController = MovieCellController(delegate: stub)
+            stub.controller = cellController
+            return cellController
+        })
+    }
 }
 
 final class MovieCellControllerStub: MovieCellControllerDelegate {
@@ -163,20 +196,20 @@ final class MovieCellControllerStub: MovieCellControllerDelegate {
     
     weak var controller: MovieCellController?
     
-     init(
+    init(
         title: String,
         overview: String,
         releaseDate: String?,
         isLoading: Bool,
         image: UIImage?
     ) {
-       viewModel = MovieViewModel(
-        title: title,
-        overview: overview,
-        releaseDate: releaseDate,
-        posterImage: image,
-        isLoading: isLoading,
-        shouldRetry: image == nil)
+        viewModel = MovieViewModel(
+            title: title,
+            overview: overview,
+            releaseDate: releaseDate,
+            posterImage: image,
+            isLoading: isLoading,
+            shouldRetry: image == nil)
     }
     
     func didRequestImageDataLoading() {
