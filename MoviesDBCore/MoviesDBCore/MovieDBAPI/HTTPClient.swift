3379368c85e0bf429064a724664750f1e94444fb
Amadou Sow <69354522+amsow@users.noreply.github.com>
2023-08-31 12:18:27 +0000

    Remove unused MoviesDBCore framework as it's replaced by MoviesCore SPM library (#30)

--- MoviesDBCore/MoviesDBCore/MovieDBAPI/HTTPClient.swift
+++
@@ -1,7 +0,0 @@
-
-import Foundation
-
-public protocol HTTPClient {
-    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
-    func request(from url: URL, completion: @escaping (Result) -> Void)
-}
